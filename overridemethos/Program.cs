4209b936729ed221a926aae04fed6f2075b6d4e1
Sookhee <s2018s38@e-mirim.hs.kr>
2020-06-02 10:26:38 +0900

    Power, SumAll 메서드 작성

--- overridemethos/Program.cs
+++ overridemethos/Program.cs
@@ -8,6 +8,40 @@ namespace overridemethos
 {
     class Program
     {
+        int Power(int input)
+        {
+            return (input*input);
+        }
+
+        int Power(int input, int count)
+        {
+            int result = 1;
+            for(int i = 1; i < count; i++)
+            {
+                result *= input;
+            }
+            return result;
+        }
+
+        int SumAll(int end)
+        {
+            int sum = 0;
+            for(int i = 0; i <= end; i++)
+            {
+                sum += i;
+            }
+            return sum;
+        }
+
+        int SumAll(int start, int end)
+        {
+            int sum = 0;
+            for(int i = start; i <= end; i++)
+            {
+                sum += i;
+            }
+        }
+
         static void Main(string[] args)
         {
         }
